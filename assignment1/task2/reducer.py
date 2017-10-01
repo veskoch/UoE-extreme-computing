6e68f4641845a08d451c7dd65064aed41aa8df2d
cholakov <cholakov@users.noreply.github.com>
2017-10-01 14:32:47 +0100

    misunderstood prompt. fixed
    prompt is leave lines which appear only once, NOT leave only once
    instance of each line

--- assignment1/task2/reducer.py
+++ assignment1/task2/reducer.py
@@ -4,19 +4,21 @@ import sys
 
 line = ""
 prev_line = ""
+single_occurence = True
 
 for line in sys.stdin:
 	line = line.strip()
 
 	if prev_line == line:
-		continue
+		single_occurence = False
 
 	else:
-		if prev_line:
+		if prev_line and single_occurence:
 			print(prev_line)
 
 		prev_line = line
+		single_occurence = True
 
 # Don't forget the last line
-if prev_line == line:
+if prev_line == line and single_occurence:
 	print(line)
