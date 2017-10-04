b33f889be161c2b0570dae0030fe5caf0e21bdbd
cholakov <cholakov@users.noreply.github.com>
2017-10-04 21:31:22 +0100

    getting it working with some improvement comprises
    - no longer using reducer-2.py
    - reducer.py completes the for loop. considering breaking out of it
    earlier

--- assignment1/task3/reducer.py
+++ assignment1/task3/reducer.py
@@ -2,8 +2,6 @@
 
 import sys
 
-bytes_found = False
-tokens_found = False
 max_bytes = 0
 max_tokens = 0
 
@@ -12,17 +10,10 @@ for entry in sys.stdin:
 	Value, Type = entry.split("\t")
 	Value = int(Value)
 
-	if Type == "bytes":
+	if Type == "bytes" and Value > max_bytes:
 		max_bytes = Value
-		bytes_found = True
 
-	if Type == "tokens":
+	if Type == "tokens" and Value > max_tokens:
 		max_tokens = Value
-		tokens_found = True
 
-	# no need to to do more iterations than necessary
-	if tokens_found and bytes_found:
-		break
-
-print("{0}\t{1}".format(max_bytes, "bytes"))
-print("{0}\t{1}".format(max_tokens, "tokens"))
+print("{0}\t{1}".format(max_bytes, max_tokens))
